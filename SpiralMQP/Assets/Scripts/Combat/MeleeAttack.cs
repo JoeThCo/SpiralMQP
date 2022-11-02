8d57ce43a5b199ad17c06e4d135ee18bc3c0067a
FrancArsen <sfang3@wpi.edu>
2022-11-01 20:00:19 -0400

    Revert "Revert "Update MeleeAttack.cs""
    This reverts commit c0a92d9e4e1791201e647eb66ddd4715e1ccad22.

--- SpiralMQP/Assets/Scripts/Combat/MeleeAttack.cs
+++ SpiralMQP/Assets/Scripts/Combat/MeleeAttack.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class MeleeAttack : MonoBehaviour {
+    public int AttackCoolDown = 2;
+    public int AttackCountDown;
+    public float AttackRange = 0.5f;
+    public Transform AttackPoint;
+    public LayerMask EnemyLayers;
+
+    void Start() {
+        AttackCountDown = AttackCoolDown;
+    }
+    void Update() {
+        AttackCountDown--;
+        if(Input.GetKeyDown(KeyCode.Q)){
+            if(AttackCountDown <= 0){
+                Attack();
+                AttackCountDown = AttackCoolDown;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Attack() {
+        Debug.Log("attack");
+        Collider2D[] hitEnemys = Physics2D.OverlapCircleAll(AttackPoint.position, AttackRange, EnemyLayers);
+        foreach(Collider2D enemy in hitEnemys){
+            Debug.Log("Hit");
+        }
+    }
+
+    void OnDrawGizmosSelected(){
+        if(AttackPoint == null)
+        return;
+        Gizmos.DrawWireSphere(AttackPoint.position,AttackRange);
     }
 }
