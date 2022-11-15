efa5d2dbe270cf7cc08de70db0c245fdd043ece5
Oliver Chen <hchen7@wpi.edu>
2022-11-15 15:11:16 -0500

    updates5

--- SpiralMQP/Assets/Scripts/UI/InventoryPage.cs
+++ SpiralMQP/Assets/Scripts/UI/InventoryPage.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 
 public class InventoryPage : MonoBehaviour
 {
+    [Header("Major Inventory Components")]
     [SerializeField] InventoryItem item;
-
     [SerializeField] RectTransform contentPanel;
+    [SerializeField] InventoryDescription itemDescription;
+    [SerializeField] MouseFollower mouseFollower;
+
+    [Header("Test")]
+    public Sprite _image;
+    public int _quantity;
+    public string _title, _description;
+
 
     List<InventoryItem> listOfItems = new List<InventoryItem>();
+
+    private void Awake() 
+    {
+        itemDescription.ResetDescription();
+    }
 
     // initialize inventory item UI with desired size
     public void InitializedInventoryUI(int inventorySize)
@@ -42,16 +55,20 @@ public class InventoryPage : MonoBehaviour
 
     private void HandleEndDrag(InventoryItem obj)
     {
-        
+        mouseFollower.Toggle(false);
     }
 
     private void HandleBeginDrag(InventoryItem obj)
     {
-        
+        mouseFollower.Toggle(true);
+        mouseFollower.SetData(_image, _quantity);
     }
 
     private void HandleItemSelection(InventoryItem obj)
     {
+        // for test
+        listOfItems[0].Select();
+        itemDescription.SetDescription(_image,_title,_description);
         Debug.Log("pass");
     }
     #endregion
@@ -59,11 +76,15 @@ public class InventoryPage : MonoBehaviour
     public void Show()
     {
         gameObject.SetActive(true);
+        itemDescription.ResetDescription();
+
+        listOfItems[0].SetData(_image,_quantity); // for test
     }
 
     // hide UI
     public void Hide()
     {
+        mouseFollower.Toggle(false);
         gameObject.SetActive(false);
     }
 }
