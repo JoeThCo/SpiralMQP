0632a0b0f472df5e3665ad25767b348b69b4fe12
FrancArsen <sfang3@wpi.edu>
2022-11-20 19:55:59 -0500

    Enemy movement

--- SpiralMQP/Assets/Scripts/Game/EnemyMovement.cs
+++ SpiralMQP/Assets/Scripts/Game/EnemyMovement.cs
@@ -2,17 +2,55 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private SpriteRenderer Sprite;
+    public GameObject PlayerObject;
+    public float MovementSpeed;
+    public Vector2 MovementDirection;
+    public float DetectDistance;
+    public float OuterDistance;
+    public float InnerDistance;
 
-    void Start(){
+    private SpriteRenderer Sprite;
+    private Rigidbody2D EnemyRigidBody;
+ 
+
+    private void Start(){
         Sprite = GetComponent<SpriteRenderer>();
+        EnemyRigidBody = GetComponent<Rigidbody2D>();
+        if(!PlayerObject){
+            PlayerObject = GameObject.FindWithTag("Player");
+        }
         UpdateSortingOrder();
     }
 
-    void UpdateSortingOrder(){
+    private void Update(){
+        float distance = Vector2.Distance(PlayerObject.transform.position, transform.position);
+        if(distance > DetectDistance){
+            MovementDirection = new Vector2();
+        } else if(distance >= OuterDistance){
+            MovementDirection = PlayerObject.transform.position - transform.position;
+            MovementDirection.Normalize();
+        } else if(distance <= InnerDistance){
+            MovementDirection = PlayerObject.transform.position - transform.position;
+            MovementDirection.Normalize();
+            MovementDirection = -MovementDirection;
+        } else {
+            MovementDirection = new Vector2();
+        }
+        
+        
+        MoveEnemy();
+    }
+
+    private void UpdateSortingOrder(){
         if (Sprite)
         {
             Sprite.sortingOrder = (int)(-transform.position.y*100.0f);
         }
     }
+
+    void MoveEnemy()
+        {
+            transform.position += (Vector3) MovementDirection * MovementSpeed * Time.deltaTime;
+            UpdateSortingOrder();
+        }
 }
