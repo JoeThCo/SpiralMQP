42e83ca30acd764d09dad17231f2ec3f0aa816f6
Oliver Chen <hchen7@wpi.edu>
2022-12-04 16:31:32 -0500

    made the build

--- SpiralMQP/Assets/Dungeon Test/Scripts2D/Generator2DEditor.cs
+++ SpiralMQP/Assets/Dungeon Test/Scripts2D/Generator2DEditor.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(Generator2D))]
-public class Generator2DEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        Generator2D generator2D = (Generator2D)target;
+// [CustomEditor(typeof(Generator2D))]
+// public class Generator2DEditor : Editor
+// {
+//     public override void OnInspectorGUI()
+//     {
+//         Generator2D generator2D = (Generator2D)target;
 
-        base.OnInspectorGUI();
+//         base.OnInspectorGUI();
 
-        if (GUILayout.Button("Generate"))
-        {
-            generator2D.Generate();
-        }
+//         if (GUILayout.Button("Generate"))
+//         {
+//             generator2D.Generate();
+//         }
 
-        if (GUILayout.Button("Clear"))
-        {
-            generator2D.Clear();
-        }
-    }
-}
+//         if (GUILayout.Button("Clear"))
+//         {
+//             generator2D.Clear();
+//         }
+//     }
+// }
