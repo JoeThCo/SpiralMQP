ac4914d799fe846a3b55b647b489077c63e96c1e
Oliver Chen <hchen7@wpi.edu>
2022-12-22 15:41:04 -0500

    Reset Project
    Reset the entire project so that I can start mess around

--- SpiralMQP/Assets/Scripts/Game/Menu.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Menu : MonoBehaviour
-{
-    [SerializeField] string MenuName;
-    public string GetMenuName() { return MenuName; }
-}
