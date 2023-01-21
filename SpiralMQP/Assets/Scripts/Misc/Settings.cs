2d2b6d9677cbe594ad2c6e4ec61dc38a937282a3
Oliver Chen <hchen7@wpi.edu>
2023-01-21 00:43:52 -0500

    update

--- SpiralMQP/Assets/Scripts/Misc/Settings.cs
+++ SpiralMQP/Assets/Scripts/Misc/Settings.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public static class Settings
 {
+    #region UNITES
+    public const float pixelsPerUnit = 16f;
+    public const float tileSizePixels = 16f;
+    #endregion
+
+
     #region DUNGEON BUILD SETTINGS
     public const int maxDungeonRebuildAttemptsForRoomGraph = 1000;
     public const int maxDungeonBuildAttempt = 10; // To prevent the dungeon generation algorithm getting into an infinite loop
@@ -33,5 +39,14 @@ public static class Settings
     public static int rollLeft = Animator.StringToHash("rollLeft");
     public static int rollRight = Animator.StringToHash("rollRight");
 
+    // Animator parameters - Door
+    public static int open = Animator.StringToHash("open");
+
+    #endregion
+
+
+    #region GAMEOBJECT TAGS
+    public const string playerTag = "Player";
+    public const string playerWeapon = "playerWeapon";
     #endregion
 }
