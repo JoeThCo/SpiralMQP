816fd06ae49d04a38fbf35031aacc969575d030e
Oliver Chen <hchen7@wpi.edu>
2023-02-05 17:50:50 -0500

    spawner added

---
+++ SpiralMQP/Assets/Scripts/Enemy/AnimateEnemy.cs
@@ -0,0 +1,142 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+[DisallowMultipleComponent]
+public class AnimateEnemy : MonoBehaviour
+{
+    private Enemy enemy;
+
+    private void Awake()
+    {
+        // Load components
+        enemy = GetComponent<Enemy>();
+    }
+
+    private void OnEnable()
+    {
+        // Subscribe to movement event
+        enemy.movementToPositionEvent.OnMovementToPosition += MovementToPositionEvent_OnMovementToPosition;
+
+        // Subscribe to idle event
+        enemy.idleEvent.OnIdle += IdleEvent_OnIdle;
+
+        // Subscribe to weapon aim event
+        //enemy.aimWeaponEvent.OnWeaponAim += AimWeaponEvent_OnWeaponAim;
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe from movement event
+        enemy.movementToPositionEvent.OnMovementToPosition -= MovementToPositionEvent_OnMovementToPosition;
+
+        // Unsubscribe from idle event
+        enemy.idleEvent.OnIdle -= IdleEvent_OnIdle;
+
+        // Unsubscribe from weapon aim event event
+        //enemy.aimWeaponEvent.OnWeaponAim -= AimWeaponEvent_OnWeaponAim;
+    }
+
+    /// <summary>
+    /// On weapon aim event handler
+    /// </summary>
+    private void AimWeaponEvent_OnWeaponAim(AimWeaponEvent aimWeaponEvent, AimWeaponEventArgs aimWeaponEventArgs)
+    {
+        InitializeAimAnimationParameters();
+        SetAimWeaponAnimationParameters(aimWeaponEventArgs.aimDirection);
+    }
+
+    /// <summary>
+    /// On movement event handler
+    /// </summary>
+    private void MovementToPositionEvent_OnMovementToPosition(MovementToPositionEvent movementToPositionEvent, MovementToPositionArgs movementToPositionArgs)
+    {
+        if (enemy.transform.position.x < GameManager.Instance.GetPlayer().transform.position.x)
+        {
+            SetAimWeaponAnimationParameters(AimDirection.Right);
+        }
+        else
+        {
+            SetAimWeaponAnimationParameters(AimDirection.Left);
+        }
+
+        SetMovementAnimationParameters();
+    }
+
+    /// <summary>
+    /// On idle event handler
+    /// </summary>
+    private void IdleEvent_OnIdle(IdleEvent idleEvent)
+    {
+        SetIdleAnimationParameters();
+    }
+
+    /// <summary>
+    /// Initialize aim animation parameters
+    /// </summary>
+    private void InitializeAimAnimationParameters()
+    {
+        enemy.animator.SetBool(Settings.aimUp, false);
+        enemy.animator.SetBool(Settings.aimUpRight, false);
+        enemy.animator.SetBool(Settings.aimUpLeft, false);
+        enemy.animator.SetBool(Settings.aimRight, false);
+        enemy.animator.SetBool(Settings.aimLeft, false);
+        enemy.animator.SetBool(Settings.aimDown, false);
+    }
+
+    /// <summary>
+    /// Set movement animation parameters
+    /// </summary>
+    private void SetMovementAnimationParameters()
+    {
+        // Set Moving
+        enemy.animator.SetBool(Settings.isIdle, false);
+        enemy.animator.SetBool(Settings.isMoving, true);
+    }
+
+
+    /// <summary>
+    /// Set idle animation parameters
+    /// </summary>
+    private void SetIdleAnimationParameters()
+    {
+        // Set idle
+        enemy.animator.SetBool(Settings.isMoving, false);
+        enemy.animator.SetBool(Settings.isIdle, true);
+    }
+
+    /// <summary>
+    /// Set aim animation parameters
+    /// </summary>
+    private void SetAimWeaponAnimationParameters(AimDirection aimDirection)
+    {
+        InitializeAimAnimationParameters();
+
+        // Set aim direction
+        switch (aimDirection)
+        {
+            case AimDirection.Up:
+                enemy.animator.SetBool(Settings.aimUp, true);
+                break;
+
+            case AimDirection.UpRight:
+                enemy.animator.SetBool(Settings.aimUpRight, true);
+                break;
+
+            case AimDirection.UpLeft:
+                enemy.animator.SetBool(Settings.aimUpLeft, true);
+                break;
+
+            case AimDirection.Right:
+                enemy.animator.SetBool(Settings.aimRight, true);
+                break;
+
+            case AimDirection.Left:
+                enemy.animator.SetBool(Settings.aimLeft, true);
+                break;
+
+            case AimDirection.Down:
+                enemy.animator.SetBool(Settings.aimDown, true);
+                break;
+        }
+    }
+}
